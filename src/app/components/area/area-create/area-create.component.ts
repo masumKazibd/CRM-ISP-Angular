4c7b44c6e18121d12f3deff322b2847b86c8342a
masumkazibd <masum.kazi.hantmk@gmail.com>
2023-12-24 19:00:18 +0600

    Package

---
+++ src/app/components/area/area-create/area-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-create',
+  templateUrl: './area-create.component.html',
+  styleUrls: ['./area-create.component.css']
+})
+export class AreaCreateComponent {
+
+}
---
+++ src/app/components/complain/complain-index/complain-index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complain-index',
+  templateUrl: './complain-index.component.html',
+  styleUrls: ['./complain-index.component.css']
+})
+export class ComplainIndexComponent {
+
+}
